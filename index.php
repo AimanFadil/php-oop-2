f08523e9b317988b977846df45ee304bed3474d8
AimanFadil <aimanbello2001@gmail.com>
2024-01-31 14:18:33 +0100

    other classes

--- index.php
+++ index.php
@@ -16,3 +16,52 @@ class Prodotto
         $this->pesci = $_pesci;
     }
 }
+
+class cibo extends prodotto
+{
+    //Property
+    public $croccantini;
+    public $scatola;
+    public $mangime;
+
+    function __construct($_cane, $_gatto, $_uccelli, $_pesci, $_croccantini, $_scatola, $_mangime)
+    {
+        parent::__construct($_cane, $_gatto, $_uccelli, $_pesci);
+
+        $this->croccantini = $_croccantini;
+        $this->scatola = $_scatola;
+        $this->mangime = $_mangime;
+    }
+}
+
+class accessorio extends prodotto
+{
+    //Property
+    public $voliera;
+    public $filtro;
+
+
+    function __construct($_cane, $_gatto, $_uccelli, $_pesci, $_voliera, $_filtro)
+    {
+        parent::__construct($_cane, $_gatto, $_uccelli, $_pesci);
+
+        $this->voliera = $_voliera;
+        $this->filtro = $_filtro;
+    }
+}
+
+class gioco extends prodotto
+{
+    //Property
+    public $mouse;
+    public $kong;
+
+
+    function __construct($_cane, $_gatto, $_uccelli, $_pesci, $_mouse, $_kong)
+    {
+        parent::__construct($_cane, $_gatto, $_uccelli, $_pesci);
+
+        $this->mouse = $_mouse;
+        $this->kong = $_kong;
+    }
+}
