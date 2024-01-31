b776e3eef2b233331b50078d04fe0259793dac6b
AimanFadil <aimanbello2001@gmail.com>
2024-01-31 15:04:40 +0100

    pushi di controllo

--- index.php
+++ index.php
@@ -34,6 +34,7 @@ class cibo extends prodotto
     }
 }
 
+
 class accessorio extends prodotto
 {
     //Property
@@ -65,3 +66,159 @@ class gioco extends prodotto
         $this->kong = $_kong;
     }
 }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-4  pt-3">
+                    <h1>PETS WORLD</h1>
+                </div>
+                <div class="col-8  pt-3">
+                    <h1>TUTTO QUELLO DI CUI HAI BISOGNO</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+
+                <div class="col-12 text-success pt-3 text-center">
+                    <h2>PRODOTTI</h2>
+                </div>
+                <div class="col-12 text-danger pt-3 text-center">
+                    <h3>CIBO</h3>
+                </div>
+                <div class="col-12 py-4">
+                    <h4>Croccantini per cani</h4>
+                </div>
+                <div class="d-flex">
+                    <div class="col-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 py-4">
+                    <h4>Scatolette per gatti</h4>
+                </div>
+                <div class="d-flex">
+                    <div class="col-12">
+                        <div class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 py-4">
+                    <h4>Mangime per pesci</h4>
+                </div>
+                <div class="d-flex">
+                    <div class="col-12">
+                        <div class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-danger pt-3 text-center">
+                    <h3>GIOCHI</h3>
+                </div>
+                <div class="col-12 py-4">
+                    <h4>Giochi per cani</h4>
+                </div>
+                <div class="d-flex">
+                    <div class="col-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 py-4">
+                    <h4>Giochi per gatti</h4>
+                </div>
+                <div class="d-flex">
+                    <div class="col-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-danger pt-3 text-center">
+                    <h3>ACCESSORI</h3>
+                </div>
+                <div class="col-12 py-4">
+                    <h4>Accessori per ucelli</h4>
+                </div>
+                <div class="d-flex">
+                    <div class="col-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 py-4">
+                    <h4>Accessori per pesci</h4>
+                </div>
+                <div class="d-flex">
+                    <div class="col-4">
+                        <div class="card" style="width: 18rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
